315736ff55e43c7b08d0f3882ce7ed9b0a620619
vmidor <76964172+vmidor@users.noreply.github.com>
2022-01-04 20:12:03 -0500

    input appears on the responde div, but not saves yet

--- my project/assets/js/index.js
+++ my project/assets/js/index.js
@@ -1,2 +1,15 @@
-var input = document.querySelector("#input");
-var addBtn = document.querySelector("#add");
+var addEl = document.querySelector("#add");
+var nameInput = document.querySelector("#name");
+var submissionResponseEl = document.querySelector("#response");
+
+// Action to be performed on click store in named function
+function showResponse(event) {
+  // Prevent default action
+  event.preventDefault();
+  console.log(event);
+  var response = nameInput.value;
+  submissionResponseEl.textContent = response;
+}
+
+// Add listener to add element
+addEl.addEventListener("click", showResponse);
